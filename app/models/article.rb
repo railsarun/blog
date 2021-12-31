d95045921d10642f3aaed3219d540781c5a914ac
railsarun <96623657+railsarun@users.noreply.github.com>
2021-12-31 15:53:16 +0530

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-validates :title, presence: true,length: {minimum:6,maximum: 50}
-validates :description, presence: true,length: {minimum:10,maximum: 50}
+validates :title, presence: true,length: {minimum:2,maximum: 50}
+validates :description, presence: true,length: {minimum:5,maximum: 50}
 
 end
